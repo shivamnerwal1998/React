ab9e1bd85c75a63884a4dc2c116882fa70985b60
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-04-26 16:57:31 +0530

    Clock.jsx

--- clock.jsx
+++ clock.jsx
@@ -1,3 +1,7 @@
+/*
+  props : as attributes in html and js 
+          props are used to set property in jsx element 
+*/ 
 import React from "react" ;
 import ReactDOM from "react-dom";
 
@@ -5,18 +9,19 @@ import ReactDOM from "react-dom";
 function Clock(props)
 {
   const element = <>
-                      <h1>Hello {props.Name}</h1>
-                      <h2>Time : {props.date.toLocaleTimeString()}</h2>
+                      <h1>Hello {props.Name}</h1>   // ek se jyada b de sakte hai 
+                      <h2>Time : {props.date.toLocaleTimeString()}</h2> // isko bade dhyaan se karna hai    
                       <h3>Byee</h3>
                   </>
-  return element ; 
+  return element ;   // return karne ka naya tareeka 
 }
 
 function display()
 {
   ReactDOM.render(
-    <Clock Name = "shivam Nerwal" date = {new Date()} />
+    <Clock Name = "shivam Nerwal" date = {new Date()} />  /* Setting props  */ 
     , document.getElementById("root")
   );
 }
-setInterval(display,1000);  
+setInterval(display,1000);  /* clock has its own timer and updates itself in every second  */
+// display function is called every second

06e96b5ef10730c538168f2f0ede0110c030cbf6
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-04-27 11:29:18 +0530

    useState() function

--- Button_events_and_hooks.jsx
+++ Button_events_and_hooks.jsx
@@ -28,3 +28,43 @@ const App= ()=>{
 export default App ;
 /* as the state  of count is changes on console but it will remain 0 where it is rendered */
 // this change in state of elements can be carried out by using hooks 
+/* STARTING WITH  HOOKS     */
+
+
+import React,{useState} from "react" ; 
+
+const App= ()=>{
+
+ /*
+    use state is a function which returns a Array with two items 
+    the first one is state object and second is  function 
+ */ 
+
+    let [count,SetCount]= useState(10) ; 
+
+    /*
+     10 initial value , count current value and 
+    by the help of setCount function we can change the value of count
+   the value which passed in setCount is current value of count 
+    */
+
+/*  change the paramerter of useSate and analyse the console  */
+    const IncCount = ()=>{
+        count++ ; 
+        SetCount(count); 
+    } 
+    const DecCount = () =>{
+        count-- ; 
+         SetCount(count); 
+    }
+
+           
+    
+    return(<div>
+            <h1>count : {count} </h1>
+            <button onClick={IncCount}>increment</button>
+            <button onClick={DecCount}>decrement</button>
+         </div>
+    ) ; 
+}
+export default App ; 

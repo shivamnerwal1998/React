e318365a204ad0514b8b3eb44f57b342c472e293
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-04-27 15:49:44 +0530

    Submitting name by button

--- Button_events_and_hooks.jsx
+++ Button_events_and_hooks.jsx
@@ -30,7 +30,38 @@ const App= ()=>{
         count-- ; 
          SetCount(count); 
     }
+/**************** submitting name by button ******************** /
+/* only taking input  */
 
+import React,{useState} from "react";
+import ReactDOM from "react-dom" ; 
+function App() {
+const [name,setName] = useState("");
+const [fullName,setFullName] = useState("") ; 
+    
+const inputEvent=(event)=>{
+    
+    setName(event.target.value);
+
+}
+const getName=()=>{
+  setFullName(name); 
+}
+    const element = <div>
+        <h1> Hello {fullName}</h1>
+         <input type="text" placeholder="Enter Your Name "
+             onChange={inputEvent}  
+             value={name}   
+         />
+         <button onClick={getName}> Display</button>
+
+
+    </div>
+    return element ; 
+}
+
+ReactDOM.render(
+<App/>,document.getElementById("root"));
            
     
     return(<div>
